7627f48a3a19bbff709b810fc936f5b1c995f3d1
LyubomiraK27 <56393590+LyubomiraK27@users.noreply.github.com>
2019-10-24 14:50:47 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i=2; i<20;++i)
+    {
+        if (i%2==0)
+            cout<<"Even"<<endl;
+        else
+            cout<<"Odd"<<endl;
+
+    }
+
+    return 0;
+}

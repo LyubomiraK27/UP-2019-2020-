0fbcefd5d1c71389bce1fcab6fafd85281a2f0f1
LyubomiraK27 <56393590+LyubomiraK27@users.noreply.github.com>
2019-10-31 23:22:05 +0200

    Add files via upload

---
+++ uprajnenie.cpp
@@ -0,0 +1,45 @@
+// uprajnenie.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double num1, num2;
+	char op;
+	cout << "Enter operator either +, -, * or /. " << endl;
+	cin >> op;
+	cout << "Enter two operands:";
+	cin >> num1 >> num2;
+
+	switch (op)
+	{
+	case '+':
+		cout << num1 + num2 << endl;
+		break;
+	case '-':
+		cout << num1 - num2 << endl;
+		break;
+	case '*':
+		cout << num1 * num2 << endl;
+		break;
+	case '/':
+		cout << num1 / num2 << endl;
+		break;
+	
+	default: //in case the input for the operator is anything different than the secified symbols
+		cout << "Invalid operator" << endl;
+
+
+		break;
+		
+	}
+
+
+
+	return 0;
+
+}
+

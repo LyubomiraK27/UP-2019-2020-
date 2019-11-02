f67232616059f5959775cba8f0f6b11964290c3c
LyubomiraK27 <56393590+LyubomiraK27@users.noreply.github.com>
2019-11-02 11:24:16 +0200

    Add files via upload

---
+++ week 4_task 3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int factorial = 1;
+    cout<<"Enter a positive integer: ";
+
+    cin>>n;
+
+
+    for (int i=1; i<=n; i++)
+       {
+
+        factorial*=i;
+       }
+   cout<<factorial<<endl;
+
+
+    return 0;
+}
